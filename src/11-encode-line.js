1a2e40badd2bc607b95388cce178d1cfc57c3c28
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-04-12 18:43:51 +0300

    feat:add solution task 11 encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  let n = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] === arr[i]) {
+      n++;
+    } else if (n === 1) {
+      result.push(arr[i]);
+    } else {
+      result.push(n + arr[i]);
+      n = 1;
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
